42e08c01d781df8594d14489cc4b7294cde8da0f
PRANAV696969 <107709470+PRANAV696969@users.noreply.github.com>
2023-05-12 10:44:38 +0530

    Update user_details.php

--- www/user_details.php
+++ www/user_details.php
@@ -72,10 +72,10 @@
         </tr>
         <?php
         // Connect to MariaDB
-        $servername = "localhost";
-        $username = "root";
-        $password = "Devops@123";
-        $dbname = "mydatabase";
+        $servername = "10.47.35.181";
+        $username = "devops";
+        $password = "devops";
+        $dbname = "devops";
 
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
